eb99210da28eb358e5886e4d6143658938617ea3
Zahid Naeem <hzahidnaeem@gmail.com>
2024-10-08 19:29:28 +0500

    path starting with '**/' was not according to the pattern of 'requestMatchers' so logic updated

--- src/main/java/com/alabtaal/library/security/SecurityConfig.java
+++ src/main/java/com/alabtaal/library/security/SecurityConfig.java
@@ -30,6 +30,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -124,9 +125,10 @@ public class SecurityConfig {
 
         .authorizeHttpRequests(auth -> {
           getAPIsWithMethod()
-              .forEach((method, paths) -> auth
-                  .requestMatchers(method, paths)
-                  .permitAll());
+              .forEach((method, paths) -> Arrays.stream(paths)
+                  .forEach(path -> auth
+                      .requestMatchers(new AntPathRequestMatcher(HttpMethod.GET.name(), path))
+                      .permitAll()));
           auth
               .requestMatchers(AUTH_WHITELIST)
               .permitAll()
